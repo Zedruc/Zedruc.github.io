77a147601fe1cdabb9410236416289e1a478ce20
Zedruc <schmidtmarkus727@gmail.com>
2021-03-02 19:47:09 +0100

    added text to menu

--- TLP/js/onArrowUp.js
+++ TLP/js/onArrowUp.js
@@ -1,5 +1,6 @@
 let arrow = document.getElementById("up-arrow");
 let menu = document.getElementById("discord-pullup");
+let listMenu = document.getElementById("list-menu");
 let closeMenuButton = document.getElementById("close-pullup-menu");
 arrow.addEventListener('click', () => {
     (menu.classList.contains("open")) ?
@@ -8,9 +9,10 @@ arrow.addEventListener('click', () => {
     (arrow.classList.contains("hidden")) ?
         arrow.classList.remove("hidden") : arrow.classList.add("hidden");
 
-
     setTimeout(() => {
         (closeMenuButton.classList.contains("hidden")) ?
             closeMenuButton.classList.remove("hidden") : closeMenuButton.classList.add("hidden");
+        (listMenu.classList.contains("hidden")) ?
+            listMenu.classList.remove("hidden") : listMenu.classList.add("hidden");
     }, 1000);
 });
