5273a37cd17e610c0d7bb319c5b3f2178042cbfe
Zedruc <schmidtmarkus727@gmail.com>
2021-03-02 08:21:49 +0100

    About Page, No invite Alert
    Added the design page layout, added an alert because there's no invite link yet

--- TLP/js/mobile.js
+++ TLP/js/mobile.js
@@ -1,3 +1,3 @@
 if (screen.width < 1920) {
-    document.body.innerHTML = "Site is being made, only 1920x1080 display supported";
+    alert("Site is being made, only 1920x1080 display supported :/");
 }
