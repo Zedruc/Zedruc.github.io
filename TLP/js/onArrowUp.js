db4568152faac4fbdb9e44ca9207f285c2b4b953
Zedruc <schmidtmarkus727@gmail.com>
2021-03-02 19:13:17 +0100

    Updated main.css, added mobile menu

--- TLP/js/onArrowUp.js
+++ TLP/js/onArrowUp.js
@@ -1,5 +1,6 @@
 let arrow = document.getElementById("up-arrow");
 let menu = document.getElementById("discord-pullup");
+let closeMenuButton = document.getElementById("close-pullup-menu");
 arrow.addEventListener('click', () => {
     (menu.classList.contains("open")) ?
         menu.classList.remove("open") : menu.classList.add("open");
@@ -7,4 +8,9 @@ arrow.addEventListener('click', () => {
     (arrow.classList.contains("hidden")) ?
         arrow.classList.remove("hidden") : arrow.classList.add("hidden");
 
+
+    setTimeout(() => {
+        (closeMenuButton.classList.contains("hidden")) ?
+            closeMenuButton.classList.remove("hidden") : closeMenuButton.classList.add("hidden");
+    }, 1000);
 });
